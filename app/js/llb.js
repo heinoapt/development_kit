502a4a329c4a5ee2f72bc193d22d63068e2fdbb7
Antti <github.TIEA4@dfgh.net>
2016-09-21 02:07:58 +0300

    Initial commit

---
+++ app/js/llb.js
@@ -0,0 +1,51 @@
+
+
+var llb_app = {}
+
+llb_app.listeners = {}
+
+
+llb_app.addListener = function(type, callback)
+{
+	if(llb_app.listeners[type] == undefined)
+	{
+		llb_app.listeners[type] = []
+
+	}
+
+	llb_app.listeners[type].push(callback)
+}
+
+llb_app.removeListener = function(type, callback)
+{
+	if(llb_app.listeners[type] != undefined)
+	{
+		if(llb_app.listeners[type].indexOf(callback) != -1)
+		{
+			llb_app.listeners[type].splice(llb_app.listeners[type].indexOf(callback),1)
+		}
+
+	}
+}
+
+window.addEventListener('message', function(event){
+	msg = JSON.parse(event.data)
+
+	if(llb_app.listeners[msg.event_type] != undefined)
+	{
+
+		for(var i=0;i<llb_app.listeners[msg.event_type].length;i++)
+		{
+
+			try
+			{
+				llb_app.listeners[msg.event_type][i](JSON.parse(msg.event_data))
+			}
+			catch(e)
+			{
+				console.log(e)
+			}
+		}
+
+	}
+})
